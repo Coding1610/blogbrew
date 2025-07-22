e5b04bcd54e84d68d088d73edeca184b53569dca
Coding1610 <yp.70010@gmail.com>
2025-07-22 15:44:37 +0530

    update code

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Footer() {
   return (
     <>
-    <div className='text-[13px] md:text-[14px] w-full flex justify-center font-roboto text-gray-700 mb-4'> © Copyright 2025 | Designed & Developed By<Link to={'https://github.com/Coding1610'} className='pl-1 font-semibold text-darkRed'> Yash Prajapati</Link> </div>
+    <div className='mt-4 text-[13px] md:text-[14px] w-full flex justify-center font-roboto text-gray-700 mb-4'> © Copyright 2025 | Designed & Developed By<Link to={'https://github.com/Coding1610'} className='pl-1 font-semibold text-darkRed'> Yash Prajapati</Link> </div>
     </>
   )
 }
